1a3962b5b50bd4c13f7a8d1c9c9e2938ede541de
alice15311 <57412796+alice15311@users.noreply.github.com>
2021-07-19 16:02:00 -0400

    created hood subsystem and hood commands

---
+++ src/main/java/frc/robot/commands/hood/SetCalcHoodAngleCommand.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.hood;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.HoodConstants;
+import frc.robot.subsystems.HoodSubsystem;
+import frc.robot.utils.Limelight;
+
+public class SetCalcHoodAngleCommand extends CommandBase {
+    private final HoodSubsystem hoodSubsystem;
+    private double encoderGoal;
+    private double power;
+    
+    public SetCalcHoodAngleCommand(HoodSubsystem hoodSubsystem, double power) {
+        this.hoodSubsystem = hoodSubsystem;
+        this.power = Math.abs(power);
+        addRequirements(hoodSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        encoderGoal = hoodSubsystem.hoodEncoderFromAngle(hoodSubsystem.calcHoodAngle(Limelight.getHorDistance()));
+        if (encoderGoal < hoodSubsystem.getEncoder()){
+            power = -power;
+        }
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("encoder goal: "+ encoderGoal + ", actual enc value: " + hoodSubsystem.getEncoder());
+        hoodSubsystem.moveHood(power);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(hoodSubsystem.calcHoodAngle(Limelight.getHorDistance()) - hoodSubsystem.hoodAngleFromEncoder(hoodSubsystem.getEncoder())) <= HoodConstants.HOOD_ANGLE_TOLERANCE;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        hoodSubsystem.stopHood();
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/HoodSubsystem.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANDigitalInput;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.HoodConstants;
+
+public class HoodSubsystem extends SubsystemBase {
+  private CANSparkMax hoodSpark;
+  private CANEncoder hoodEncoder;
+  private CANDigitalInput beamBreak;
+
+  public HoodSubsystem() {
+    hoodSpark = new CANSparkMax(HoodConstants.HOOD_SPARK, MotorType.kBrushless);  
+    hoodEncoder = hoodSpark.getEncoder();
+    hoodSpark.setInverted(true);
+    beamBreak = hoodSpark.getReverseLimitSwitch(CANDigitalInput.LimitSwitchPolarity.kNormallyClosed);
+    hoodSpark.setSoftLimit(SoftLimitDirection.kForward, (float)hoodEncoderFromAngle(HoodConstants.MAX_HOOD_ANGLE));
+    hoodSpark.setSoftLimit(SoftLimitDirection.kReverse, (float)hoodEncoderFromAngle(0));
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  public void moveHood(double power) {
+    hoodSpark.set(power);
+  }
+
+  public void stopHood() {
+    hoodSpark.set(0);
+  }
+
+  public double hoodEncoderFromAngle(double angle){
+    return HoodConstants.COUNTS_PER_DEGREE * angle;
+  }
+
+  public double hoodAngleFromEncoder(double encoderValue){
+    return encoderValue / HoodConstants.COUNTS_PER_DEGREE;
+  }
+
+  public void resetEncoder(){
+    hoodEncoder.setPosition(0);
+}
+
+  public double getEncoder(){
+    return hoodEncoder.getPosition();
+  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
+  public boolean getBeamBreak(){
+    return beamBreak.get();
+  }
+
+   public double calcHoodAngle(double distance) {
+     //y = mx+b values based on hood testing
+     return ((.0867898* distance) + 12.4589);
+   }
+}
